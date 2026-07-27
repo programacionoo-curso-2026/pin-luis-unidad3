d4e80fec68948c84f0ce3a1afd608ff8a385b92e
PIN TRUJILLO LUIS <lupintr@uide.edu.ec>
2026-07-27 10:34:21 -0500

    int8

--- taller22-goroutines/main.go
+++ taller22-goroutines/main.go
@@ -79,3 +79,28 @@ func main() {
 
 	fmt.Print("Todas las operaciones completadas. Saliendo")
 }
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	orders := generateOrders(20)
+
+	go func() {
+		defer wg.Done()
+		processOrders(orders)
+	}()
+
+	go func() {
+		defer wg.Done()
+		updateOrderStatuses(orders)
+	}()
+
+	go func() {
+		defer wg.Done()
+		reportOrderStatus(orders)
+	}()
+
+	wg.Wait()
+
+	fmt.Print("Todas las operaciones completadas. Saliendo")
+}
